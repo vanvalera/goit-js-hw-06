2e616deadfb330b9350bafab965e404afff6b1be
vanvalera <vmarton777@gmail.com>
2023-02-27 22:04:18 +0200

    firstTaskComplete

---
+++ src/js/task-01.js
@@ -0,0 +1,10 @@
+const categories = document.querySelector('#categories');
+const items = categories.querySelectorAll('.item');
+console.log(`Number of categories: ${items.length}`);
+items.forEach((item) => {
+    const header = item.querySelector('h2');
+    const itemCount = item.querySelectorAll('li').length;
+    console.log(`Category: ${header.textContent}`);
+    console.log(`Elements: ${itemCount}`);
+}
+);
