d08291da19320bdafb741f0a7d5dd5ac04026ff0
vanvalera <vmarton777@gmail.com>
2023-03-04 12:16:05 +0200

    complete-task-8

--- src/js/task-08.js
+++ src/js/task-08.js
@@ -17,25 +17,3 @@ function onFormSubmit(event) {
   console.log(formData);
   form.reset();
 }
-// const loginForm = document.querySelector('.login-form');
-
-// loginForm.addEventListener('submit', e => {
-//   e.preventDefault(); // перевірка форми без перезавантаження сторінки
-
-//   const emailInput = loginForm.elements.email;
-//   const passwordInput = loginForm.elements.password;
-
-//   if (emailInput.value === '' || passwordInput.value === '') {
-//     alert('Будь ласка, заповніть всі поля форми.');
-//     return;
-//   }
-
-//   const formData = {
-//     email: emailInput.value,
-//     password: passwordInput.value,
-//   };
-
-//   console.log(formData);
-
-//   loginForm.reset(); // очищення форми
-// });
