e3ee3114fbd2ddb44ee0d24a9a1ae0cc6d444640
vanvalera <vmarton777@gmail.com>
2023-03-06 22:23:38 +0200

    complete-task-9

--- src/js/task-09.js
+++ src/js/task-09.js
@@ -1,5 +1,13 @@
+const btnChangeColor = document.querySelector('.change-color');
+const curentColorValue = document.querySelector('.color');
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+btnChangeColor.addEventListener('click', () => {
+  const randomColor = getRandomHexColor();
+  document.body.style.backgroundColor = randomColor;
+  curentColorValue.textContent = randomColor;
+});
